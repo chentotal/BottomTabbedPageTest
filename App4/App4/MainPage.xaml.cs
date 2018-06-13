8dd61113b4376787ce34c8642ee1b27f1a17ca47
ChenTotal <chen.total@outlook.com>
2018-06-13 14:38:51 +0800

    Upload source code

---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Naxam;
+using Naxam.Controls.Forms;
+
+namespace App4
+{
+	public partial class MainPage: BottomTabbedPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
